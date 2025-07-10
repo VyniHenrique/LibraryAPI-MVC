a35e1f3866be07c4ddcdc5756ce6d01466ba0ff7
Vynicius <vynicius.henrique15@gmail.com>
2025-07-09 22:28:55 -0300

    adicionando o spring security na aplicacao

---
+++ src/main/java/br/com/vyniciushenrique/LibraryAPI/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.vyniciushenrique.LibraryAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(autorize -> {
+                    autorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+}
