88ccad7a1ec49fa55b11303430b0581d6a80e7ad
Vynicius <vynicius.henrique15@gmail.com>
2025-07-10 22:44:45 -0300

    adicionando um form login personalizado

---
+++ src/main/java/br/com/vyniciushenrique/LibraryAPI/controller/LoginViewController.java
@@ -0,0 +1,13 @@
+package br.com.vyniciushenrique.LibraryAPI.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
+}
