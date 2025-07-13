debea6c1d85782982923a28a8528ad2757527736
Vynicius <vynicius.henrique15@gmail.com>
2025-07-12 21:13:04 -0300

    criando as tabelas para autenticacao

--- src/main/java/br/com/vyniciushenrique/LibraryAPI/config/SecurityConfiguration.java
+++ src/main/java/br/com/vyniciushenrique/LibraryAPI/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package br.com.vyniciushenrique.LibraryAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,9 +28,36 @@ public class SecurityConfiguration {
                     configurer.loginPage("/login").permitAll();
                 })
                 .authorizeHttpRequests(autorize -> {
+
+                    autorize.requestMatchers("/autores/**").hasRole("ADMIN");
+                    autorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+
                     autorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user1 = User.builder()
+                .username("usuario1")
+                .roles("USER")
+                .password(encoder.encode("123"))
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("usuario2")
+                .roles("ADMIN")
+                .password(encoder.encode("321"))
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 }
