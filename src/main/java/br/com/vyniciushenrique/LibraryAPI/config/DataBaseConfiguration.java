79ec493dee927ae7f1ddea43118a59b812088f33
Vynicius <vynicius.henrique15@gmail.com>
2025-07-28 15:40:23 -0300

    adicionando o arquivo dockerfile no projeto

--- src/main/java/br/com/vyniciushenrique/LibraryAPI/config/DataBaseConfiguration.java
+++ src/main/java/br/com/vyniciushenrique/LibraryAPI/config/DataBaseConfiguration.java
@@ -2,6 +2,7 @@ package br.com.vyniciushenrique.LibraryAPI.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DataBaseConfiguration {
 
     @Value("${spring.datasource.url}")
@@ -37,6 +39,9 @@ public class DataBaseConfiguration {
         config.setIdleTimeout(10000);
         config.setMinimumIdle(1);
         config.setPoolName("library-db-pool");
+
+        log.trace("Data souce aberto com o username: {} e a senha: {}", config.getUsername(), config.getPassword());
+        log.info("Url do banco atual: {}", url);
 
         return new HikariDataSource(config);
     }
